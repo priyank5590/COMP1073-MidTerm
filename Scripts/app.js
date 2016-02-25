a69c9d375055adbe6d1db765bb4caa741be5df22
priyank5590 <vyaspriyank5@gmail.com>
2016-02-25 06:35:37 -0800

    Spetses,Amorgos,Syros para. added

--- Scripts/app.js
+++ Scripts/app.js
@@ -18,6 +18,21 @@ console.log("App Started...")
 	var Alonissos;
 	Alonissos = document.getElementById("Alonissos");
 	Alonissos.innerHTML="Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream. The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes. Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy."
+	
+	// Spetses Paragraph
+	var Spetses;
+	Spetses = document.getElementById("Spetses");
+	Spetses.innerHTML="This bohemian paradise is more Côte d'Azur than Grecian island. Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques. Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight."
+	
+	// Amorgos Paragraph
+	var Amorgos;
+	Amorgos = document.getElementById("Amorgos");
+	Amorgos.innerHTML="Shipwrecks, sea caves and beautiful clear waters make Amorgos popular with snorkelers, but the mountainous island is the perfect compromise between unwinding and adventure, boasting beautiful beaches but also hiking, scuba diving and rock-climbing. One of the island's main attractions is the 11th-century cliffside monastery of Panayia Hozoviótissa, which is precariously carved into a cliff."
+	
+	// Syros Paragraph
+	var Syros;
+	Syros = document.getElementById("Syros");
+	Syros.innerHTML="A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners. Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways."
 
 })();
 
