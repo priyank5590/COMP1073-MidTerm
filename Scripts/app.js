28946449a3723a69c7b95ff52c6f419aea939278
priyank5590 <vyaspriyank5@gmail.com>
2016-02-25 06:19:34 -0800

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,18 @@
+// setup your IIFE (Immediately Invoked Function Expression)
+var travelReport = (function () {
+
+"use strict";
+console.log("App Started...")
+
+// Info Paragraph
+	var IntroParagraph;
+	IntroParagraph = document.getElementById("IntroParagraph");
+	IntroParagraph.innerHTML="Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. Greece has 1,400 islands, though only 230 of them are inhabited. And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too."
+
+// Info Paragraph
+	var IntroParagraph;
+	IntroParagraph = document.getElementById("IntroParagraph");
+	IntroParagraph.innerHTML="Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. Greece has 1,400 islands, though only 230 of them are inhabited. And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too."
+
+})();
+
